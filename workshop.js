c31e61b4f6ec5d5e3dd9254dc6d243b33a0c5959
Katie Barrett <kayt.barrett22@gmail.com>
2023-12-20 15:11:57 -0500

    problem 2 complete

--- workshop.js
+++ workshop.js
@@ -1,3 +1,4 @@
+//Problem 1
 //REACTO
 //Restate: check that input string s has valid brackets and parentheses, as in all opening brackets/parentheses
 //have closing brackets/parentheses and they all close in the correct order
@@ -35,6 +36,42 @@ function isValidBrackets(s) {
 
 }
 
+//Example:
 console.log(isValidBrackets("()")); // Output: true
 console.log(isValidBrackets("]] [[")); // Output: false
-console.log(isValidBrackets("there (may [be] text){between [y]o[u]r (brackets)}")); 
+console.log(isValidBrackets("there (may [be] text){between [y]o[u]r (brackets)}")); //Output: true
+
+
+//Problem 2:
+//REACTO
+//Restate: there is an array of ticket amounts, and each index represents one person who would like to buy
+//that amount of tickets. calculate the time required for a person at a specific index (k) to buy all their tickets
+//it takes one second to buy a ticket, then that person moves to the back of the line
+//Ex: tickets = [2, 3, 2] k=2, to iterate through the line until person 3 (k=2 of a 0 index array) buys 2 tickets
+//would be a time of 6 seconds
+//Approach: i think a while loop, for while tickets[k] > 0 is necessary to loop through the array while that remains true
+//within that while loop, I think a for loop to add time for each iteration through the array/line is needed
+//Code: 
+
+function ticketTime(tickets, k){
+    const remainingTickets = [...tickets]
+    let time = 0
+
+    while (remainingTickets[k] > 0 ){
+        for(let i = 0; i < remainingTickets.length; i++){
+            if(remainingTickets[i] > 0) {
+                remainingTickets[i]--;
+                time++;
+            }  
+        }
+    }
+
+    return time;
+}
+
+// Example
+const tickets = [2, 3, 2];
+const k = 2;
+const result = ticketTime(tickets, k);
+console.log(result); // Output: 6
+
